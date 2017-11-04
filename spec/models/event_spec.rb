d3c61f2557a5ef0da751067ef0834812f5fc7f83
samuelssnider <sam_redins@yahoo.com>
2017-11-04 15:20:15 -0600

    event relationships

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,36 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  describe "valid attributes"
-  it "is valid with valid attributes" do
-    event = Event.create(title: "thing",
-                         description: "this is a thing that happened")
-    expect(event.save).to be true
-  end
-  
-  it "is invalid without a title" do
-    event = Event.create(description: "this is a thing that happened")
-    expect(event.save).to be false
-  end
-  
-  it "is invalid without a description" do
-    event = Event.create(title: "thing")
-    expect(event.save).to be false
-  end
-  
-  it "is invalid without a unique title" do
-    event_1 = Event.create(title: "thing",
+  describe "valid attributes" do
+    it "is valid with valid attributes" do
+      event = Event.create(title: "thing",
                            description: "this is a thing that happened")
-    event_2 = Event.create(title: "thing",
-                           description: "this is a second thing that happened")
-    expect(event_2.save).to be false
+      expect(event.save).to be true
+    end
+    
+    it "is invalid without a title" do
+      event = Event.create(description: "this is a thing that happened")
+      expect(event.save).to be false
+    end
+    
+    it "is invalid without a description" do
+      event = Event.create(title: "thing")
+      expect(event.save).to be false
+    end
+    
+    it "is invalid without a unique title" do
+      event_1 = Event.create(title: "thing",
+                             description: "this is a thing that happened")
+      event_2 = Event.create(title: "thing",
+                             description: "this is a second thing that happened")
+      expect(event_2.save).to be false
+    end
+    
+    it "is invalid without a unique description" do
+      event_1 = Event.create(title: "thing",
+                             description: "this is a thing that happened")
+      event_2 = Event.create(title: "thing two",
+                             description: "this is a thing that happened")
+      expect(event_2.save).to be false
+    end
   end
-  
-  it "is invalid without a unique description" do
-    event_1 = Event.create(title: "thing",
+  describe "relationships" do
+		it "responds to deities" do
+      event = Event.create(title: "thing",
                            description: "this is a thing that happened")
-    event_2 = Event.create(title: "thing two",
-                           description: "this is a thing that happened")
-    expect(event_2.save).to be false
-  end
+      
+			expect(event).to respond_to(:deities)
+      expect(event).to respond_to(:event_actors)
+		end
+	end
 end
