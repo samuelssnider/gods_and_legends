3e444cbe7abfebe1e9450d222b59f3c63a8c6e3e
samuelssnider <sam_redins@yahoo.com>
2017-11-04 23:25:12 -0600

    updates to books and views

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,12 @@
 class Book
-  attr_reader :link, :title, :authors, :publisher, :image
+  attr_reader :link, :title, :authors, :publisher, :image, :store_link
   def initialize(attrs)
-    @link      = attrs[:selfLink]
-    @title     = attrs[:volumeInfo][:title]
-    @authors   = attrs[:volumeInfo][:authors]
-    @publisher = attrs[:volumeInfo][:publisher]
-    @image     = attrs[:volumeInfo][:imageLinks][:thumbnail] if attrs[:volumeInfo][:imageLinks]
+    @api_link      = attrs[:selfLink]
+    @title      = attrs[:volumeInfo][:title]
+    @authors    = attrs[:volumeInfo][:authors]
+    @publisher  = attrs[:volumeInfo][:publisher]
+    @image      = attrs[:volumeInfo][:imageLinks][:thumbnail] if attrs[:volumeInfo][:imageLinks]
+    @store_link = attrs[:volumeInfo][:infoLink]
   end
 end
      
