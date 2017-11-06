a58ca6c640f9c58d3a25d039139be2d6605ab455
samuelssnider <sam_redins@yahoo.com>
2017-11-06 16:34:59 -0700

    fixed

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module GodsAndLegends
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    congig.serve_static_assets = true
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
