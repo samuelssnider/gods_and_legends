43c28cf93be786179076f49aa3daf80c9704c82a
samuelssnider <sam_redins@yahoo.com>
2017-11-07 14:07:13 -0700

    asset pipeline

--- app/controllers/mythologies/deities_controller.rb
+++ app/controllers/mythologies/deities_controller.rb
@@ -2,7 +2,6 @@ module Mythologies
   class DeitiesController < ApplicationController
     def index
       @mythology = Mythology.find_by(name: params["name"].capitalize)
-      binding.pry
       @deities = @mythology.deities
     end
   end
