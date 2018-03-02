8882b827a545723696c851ab060aa2d2e1b43999
samuelssnider <sam_redins@yahoo.com>
2018-03-01 17:20:44 -0700

    added mythology to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108201316) do
+ActiveRecord::Schema.define(version: 20180302001037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20171108201316) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "mythology_id"
+    t.index ["mythology_id"], name: "index_events_on_mythology_id"
   end
 
   create_table "mythologies", force: :cascade do |t|
@@ -85,4 +87,5 @@ ActiveRecord::Schema.define(version: 20171108201316) do
   add_foreign_key "deity_domains", "domains"
   add_foreign_key "event_actors", "deities"
   add_foreign_key "event_actors", "events"
+  add_foreign_key "events", "mythologies"
 end
