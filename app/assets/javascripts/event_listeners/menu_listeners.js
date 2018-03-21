826aa0c0ea6784bb6cfb5f236f86480b22314705
samuelssnider <sam_redins@yahoo.com>
2018-03-20 18:20:22 -0600

    add to dropdown, links appearing but not in the correct order

--- app/assets/javascripts/event_listeners/menu_listeners.js
+++ app/assets/javascripts/event_listeners/menu_listeners.js
@@ -5,4 +5,21 @@ var dropDown = function() {
 
 $(document).ready(function(){
   $('button[name="dd-nav-button"]').on('click', dropDown);
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 })
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
