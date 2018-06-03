72cc21556b130b32462db2f430afba2b066112ff
samuelssnider <sam_redins@yahoo.com>
2018-06-03 17:55:09 -0600

    add full/half siblings to deities.siblings

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 20180302001037) do
   create_table "births", force: :cascade do |t|
     t.integer "parent_id"
     t.integer "child_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "deities", force: :cascade do |t|
