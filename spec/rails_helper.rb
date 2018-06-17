98529d05a54fb340c7508afb1bb8621cf3edb92e
samuelssnider <sam_redins@yahoo.com>
2018-06-17 14:56:22 -0600

    factory bot replacement

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,10 +9,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rspec'
 
+
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-
 ActiveRecord::Migration.maintain_test_schema!
+
+FactoryBot.find_definitions
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
