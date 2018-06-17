8d1ccbe01c0a13b2f725e7259d7082666125e5ba
samuelssnider <sam_redins@yahoo.com>
2018-06-17 14:59:42 -0600

    test cleanup, really need to change static values to values found by instances

--- spec/requests/event_request_spec.rb
+++ spec/requests/event_request_spec.rb
@@ -29,7 +29,7 @@ describe "when i send a get request to api-v1-events" do
     json_deduced = JSON.parse(response.body)
     
     expect(response).to have_http_status(200)
-    expect(json_deduced.count).to eq(5)
+    expect(json_deduced.count).to eq(6)
     expect(json_deduced["title"]).to eq(event_1.title)
     expect(json_deduced["description"]).to eq(event_1.description)
   end
@@ -45,7 +45,7 @@ describe "when i send a get request to api-v1-events" do
     
     json_deduced = JSON.parse(response.body)
     expect(response).to have_http_status(200)
-    expect(json_deduced.count).to eq(5)
+    expect(json_deduced.count).to eq(6)
     expect(json_deduced.first.count).to eq(10)
     expect(json_deduced.first["name"]).to eq(deity_1.name)
     expect(json_deduced.first["description"]).to eq(deity_1.description)
