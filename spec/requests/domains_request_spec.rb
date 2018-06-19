ec40d9bb8d330ac56e7c550948baac88083db578
samuelssnider <sam_redins@yahoo.com>
2018-06-19 10:31:42 -0600

    add domain deities request (multiple deities)

--- spec/requests/domains_request_spec.rb
+++ spec/requests/domains_request_spec.rb
@@ -15,6 +15,7 @@ describe "when i send a get request to api/v1/domains" do
     expect(json_deduced.first["name"]).to eq(domain_1.name)
     expect(json_deduced.last["name"]).to eq(domain_2.name)
   end
+  
   it "and namespace with /:domain_id, I should recieve a 200 respose with a single domain" do
     domains = create_list(:domain, 10)
     domain_1 = Domain.first
@@ -26,6 +27,7 @@ describe "when i send a get request to api/v1/domains" do
     expect(response).to have_http_status(200)
     expect(json_deduced["name"]).to eq(domain_1.name)
   end
+  
   it "and namespace with /:domain_id/deities, I should recieve a 200 respose with a single deity related to that domain" do
     domain = create(:domain)
     deity  = create(:deity)
@@ -39,6 +41,26 @@ describe "when i send a get request to api/v1/domains" do
     expect(response).to have_http_status(200)
     expect(json_deduced.first["name"]).to eq(deity.name)
     expect(json_deduced.first["description"]).to eq(deity.description)
+  end
+  
+  it "and namespace with /:domain_id/deities, I should recieve a 200 respose with a single deity related to that domain" do
+    domain   = create(:domain)
+    deities  = create_list(:deity, 10)
+    deity_1  = Deity.first
+    deity_2  = Deity.last
+    deities.each do |deity|
+      deity.domains << domain
+    end
+    
+    get "/api/v1/domains/#{domain.id}/deities"
+    
+    json_deduced = JSON.parse(response.body)
+    
+    expect(response).to have_http_status(200)
+    expect(json_deduced.first["name"]).to eq(deity_1.name)
+    expect(json_deduced.first["description"]).to eq(deity_1.description)
+    expect(json_deduced.last["name"]).to eq(deity_2.name)
+    expect(json_deduced.last["description"]).to eq(deity_2.description)
   end
 end
 
