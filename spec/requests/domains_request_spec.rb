a39d43f77f4bd2978589bfc9645142cebf916d42
samuelssnider <sam_redins@yahoo.com>
2018-06-19 08:48:14 -0600

    single domain request spec added

--- spec/requests/domains_request_spec.rb
+++ spec/requests/domains_request_spec.rb
@@ -3,5 +3,28 @@ require 'rails_helper'
 describe "when i send a get request to api/v1/domains" do
   it "I should recieve a 200 respose with all the domains" do
     domains = create_list(:domain, 10)
+    domain_1 = Domain.first
+    domain_2 = Domain.last
+    
+    get '/api/v1/domains'
+    
+    json_deduced = JSON.parse(response.body)
+    
+    expect(response).to have_http_status(200)
+    expect(json_deduced.count).to eq(10)
+    expect(json_deduced.first["name"]).to eq(domain_1.name)
+    expect(json_deduced.last["name"]).to eq(domain_2.name)
+  end
+  it "and namespace with /:domain_id, I should recieve a 200 respose with a single domain" do
+    domains = create_list(:domain, 10)
+    domain_1 = Domain.first
+    
+    get "/api/v1/domains/#{domain_1.id}"
+    
+    json_deduced = JSON.parse(response.body)
+    
+    expect(response).to have_http_status(200)
+    expect(json_deduced["name"]).to eq(domain_1.name)
   end
 end
+
