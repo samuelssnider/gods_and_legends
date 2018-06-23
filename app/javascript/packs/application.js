deeb33dc9388e484c282b92035724ae4f610350b
samuelssnider <sam_redins@yahoo.com>
2018-06-22 23:49:30 -0600

    navbar working,  not pretty but functional

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require_tree .  
+
 console.log('Hello World from Webpacker')
