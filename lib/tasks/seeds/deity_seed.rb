06a5cd487cb97c5e4369877ed1f901565b4dd9d7
samuelssnider <sam_redins@yahoo.com>
2018-06-30 14:50:18 -0600

    work on sepatate rake tasks (deity)

---
+++ lib/tasks/seeds/deity_seed.rb
@@ -0,0 +1,17 @@
+require 'csv'
+require 'json'
+require ''
+class DeitySeed
+  def self.seed
+    deities = CSV.foreach "./db/data/deities.csv", headers: true, header_converters: :symbol
+    deities.each do |row|
+      row = row.to_h
+      row[:tier] = row[:tier].to_i
+      row[:gender] = row[:gender].to_i
+      row[:mythology_id] = row[:mythology_id].to_i
+      row[:image] = "deities/#{row[:name]}.jpg"
+      puts row[:name]
+      Deity.create!(row)
+    end
+  end
+end
