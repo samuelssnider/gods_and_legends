ff08d822d46603dc2d698531339b852ed0cc8a82
samuelssnider <sam_redins@yahoo.com>
2019-01-18 08:27:48 -0700

    add to domain model testing (all except booksearch)

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe Domain, type: :model do
       domain = create(:domain)
       expect(domain.save).to be true
     end
+    it {should validate_presence_of(:name) }
+    it {should validate_uniqueness_of(:name) }
+  end
+  
+  describe "Relationships" do
+    it { should have_many(:deity_domains) }
+    it { should have_many(:deities).through(:deity_domains) }
   end
   
 end
